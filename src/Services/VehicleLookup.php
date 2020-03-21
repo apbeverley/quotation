b04fd50f45c86d338f25e5559fb2e56c1dff3090
Andrew Beverley <andrew@andrewbeverley.co.uk>
2020-03-21 23:08:44 +0000

    vehicle api service lookup

---
+++ src/Services/VehicleLookup.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class VehicleLookup
+{
+    public function getVehicleAbi(string $registration): string
+    {
+        $abiCodes = [
+            '22529902',
+            '46545255',
+            '52123803',
+            '' //failed response if no lookup was found
+        ];
+
+        return $abiCodes[array_rand($abiCodes)];
+    }
+}
