2ad83a1534e3a099447ce65a61d9f94b6e52b9a3
Andrew Beverley <andrew@andrewbeverley.co.uk>
2020-03-22 22:57:50 +0000

    Add a fake request for vehicle lookip

--- src/Services/VehicleLookupService.php
+++ src/Services/VehicleLookupService.php
@@ -2,8 +2,39 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
 class VehicleLookupService
 {
+    public const VEHICLE_LOOKUP_API_URL = '';
+
+    public function request(string $regNo)
+    {
+        $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => SELF::VEHICLE_LOOKUP_API_URL,
+            // You can set any number of default request options.
+            'timeout' => 2.0,
+            // You should turn HTTP error exceptions off so that this package can handle all HTTP return codes.
+            'http_errors' => false,
+            // Get the response as json
+            RequestOptions::HEADERS => [
+                'Content-Type' => 'application/json'
+            ],
+        ]);
+
+        $response = $client->request('POST', '/vehicle-details', [
+            RequestOptions::JSON => ['regNo' => $regNo]
+        ]);
+
+        $statusCode = (int)$response->getStatusCode();
+
+        if ($statusCode === 200) {
+            return $response->getBody()->getContents();
+        }
+    }
+
     public function getVehicleAbi(string $registration): string
     {
         $abiCodes = [
